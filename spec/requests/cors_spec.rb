8e5d707517f0060a9e7f540096d8ed85ad2b20c8
Vince Tse <vincetse@users.noreply.github.com>
2026-02-06 22:46:19 -0800

    Rails 8.1

--- spec/requests/cors_spec.rb
+++ spec/requests/cors_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe 'Todos API', type: :request do
   describe 'Check CORS headers' do
-    headers = { 'Origin' => 'example.com' }
+    headers = { 'HTTP_ORIGIN' => 'http://example.com' }
     before { get '/todos', :headers => headers }
     it 'returns CORS headers' do
       expect(response.headers['Access-Control-Allow-Origin']).to eq '*'
